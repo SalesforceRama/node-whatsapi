9f44a1b285542ba1cdf270c08773ced9707ead40
Matteo Contrini <m.contrini@gmail.com>
2015-06-11 20:17:06 +0200

    Fixed sendMessageReceipt

--- extensions/service.js
+++ extensions/service.js
@@ -79,7 +79,7 @@ WhatsApi.prototype.sendMessageReceipt = function(message, type) {
 		attributes['participant'] = message['author'];
 	}
 
-	return new protocol.Node('receipt', attributes);
+	this.sendNode(protocol.Node('receipt', attributes));
 };
 
 /**
